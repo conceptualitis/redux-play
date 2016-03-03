118aa38f20fb21ace32cf1359609453ef30f6016
Neil Duffy <neiladuffy@gmail.com>
2016-03-03 00:06:04 -0600

    team pages

--- app/client.js
+++ app/client.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import TeamList from './components/team-list';
+import TeamPage from './components/team-page';
 
 render((
-    // <TeamList/>
     <Router history={browserHistory}>
       <Route path="/" component={TeamList} />
+      <Route path="/teams/:id" component={TeamPage} />
     </Router>
   ),
   document.getElementById('app')
